217d0c8a59d69c1bb141c0577b458a30eb8bb8e2
oscar-valen115 <oscar.valen115@gmail.com>
2021-05-07 15:48:48 -0400

    patient update finished

--- curl-scripts/mangos/create.sh
+++ curl-scripts/mangos/create.sh
@@ -4,6 +4,7 @@ curl "http://localhost:8000/mangos/" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
+  --header "Accept: application/json; indent=4" \
   --header "Authorization: Token ${TOKEN}" \
   --data '{
     "mango": {
